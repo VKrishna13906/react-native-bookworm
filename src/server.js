83bcf4e377a3979e99233dfe77cc202774ae7846
VKrishna13906 <krishnavishwakarma13906@gmail.com>
2025-08-24 18:15:51 +0530

    Adding a cron

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js"
 import bookRoutes from "./routes/bookRoutes.js"
 import {connectDB} from "./lib/db.js"
+import job from "./lib/cron.js";
 import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
